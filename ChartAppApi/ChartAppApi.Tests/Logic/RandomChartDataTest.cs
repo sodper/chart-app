553116cdd73bf4b6c15e609134e651bbd1e122ce
Per Flitig <per.flitig@gmail.com>
2017-11-07 23:59:23 +0100

    - Implemented the random chart data generator with tests

---
+++ ChartAppApi/ChartAppApi.Tests/Logic/RandomChartDataTest.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.RegularExpressions;
+using ChartAppApi.Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChartAppApi.Tests.Logic
+{
+    [TestClass]
+    public class RandomChartDataTest
+    {
+        [TestMethod]
+        public void GetChart_RandomNumberOfItems()
+        {
+            var data1 = new RandomChartData(0);
+            var noOfDataPoints1 = data1.GetChart().Data.Length;
+            var data2 = new RandomChartData(1);
+            var noOfDataPoints2 = data2.GetChart().Data.Length;
+
+            Assert.AreNotEqual(noOfDataPoints1, noOfDataPoints2);
+        }
+
+        [TestMethod]
+        public void GetChart_LabelsAreAlphaNumericChars()
+        {
+            var data = new RandomChartData(5);
+            var chart = data.GetChart();
+            var regex = new Regex(@"[A-Za-z0-9]");
+
+            foreach (var label in chart.Labels)
+            {
+                Assert.IsTrue(regex.IsMatch(label));
+            }
+        }
+
+        [TestMethod]
+        public void GetChart_DataPointsArePositiveIntegersLessThan3000()
+        {
+            var data = new RandomChartData(7);
+            var chart = data.GetChart();
+
+            foreach (var dataPoint in chart.Data)
+            {
+                Assert.IsTrue(dataPoint > 0);
+                Assert.IsTrue(dataPoint < 3000);
+            }
+        }
+
+        [TestMethod]
+        public void GetChart_ColorsAreKnownColors()
+        {
+            var data = new RandomChartData(9);
+            var chart = data.GetChart();
+
+            foreach (var color in chart.Colors)
+            {
+                Assert.IsTrue(color.IsKnownColor);
+            }
+        }
+    }
+}
--- ChartAppApi/ChartAppApi/Logic/RandomChartData.cs
+++ ChartAppApi/ChartAppApi/Logic/RandomChartData.cs
@@ -1,18 +1,46 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using ChartAppApi.Models;
 
 namespace ChartAppApi.Logic
 {
     public class RandomChartData : IChartData
     {
+        private readonly Random _rng;
+        private readonly string _labelData = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        private const int MaxNoOfDataPoints = 15;
+        private const int MaxDataValue = 3000;
+
+        public RandomChartData()
+        {
+            _rng = new Random();
+        }
+
+        public RandomChartData(int seed)
+        {
+            _rng = new Random(seed);
+        }
+
         public Chart GetChart()
         {
-            return new Chart
+            var noOfDatapoints = _rng.Next(1, MaxNoOfDataPoints);
+            var randomChart = Chart.CreateChart(noOfDatapoints);
+
+            for (var i = 0; i < noOfDatapoints; i++)
             {
-                Colors = new []{ Color.Bisque, Color.Aqua },
-                Data = new [] { 142, 133 },
-                Labels = new [] { "Jan", "Feb" }
-            };
+                randomChart.Colors[i] = GenerateKnownColor();
+                randomChart.Data[i] = _rng.Next(0, MaxDataValue);
+                randomChart.Labels[i] = _labelData[_rng.Next(0, _labelData.Length-1)].ToString();
+            }
+
+            return randomChart;
+        }
+
+        private Color GenerateKnownColor()
+        {
+            var names = (KnownColor[])Enum.GetValues(typeof(KnownColor));
+            var randomColorName = names[_rng.Next(names.Length)];
+            return Color.FromKnownColor(randomColorName);
         }
     }
 }
